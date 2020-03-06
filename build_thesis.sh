6f426120b372a786e6b8cbb95e7556bc0aece752
npklein <niekdeklein@gmail.com>
2020-03-06 13:03:51 +0100

    reorganize and first version introduction

---
+++ build_thesis.sh
@@ -0,0 +1,6 @@
+pdflatex thesis.tex;
+bibtex chapters/chapter1-introduction/chapter1-introduction;
+bibtex chapters/chapter2-genetic-architecture/chapter2-genetic-architecture;
+bibtex chapters/chapter4-deconvolution/chapter4-deconvolution;
+pdflatex thesis.tex;
+pdflatex thesis.tex
--- run.sh
+++
@@ -1,6 +0,0 @@
-latex thesis.tex
-bibtex thesis
-latex thesis.tex
-latex thesis.tex
-dvips -t a5 thesis.dvi
-ps2pdf thesis.ps 
